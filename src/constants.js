3f1027d0dc8f9dad8d5b8a87389d44e7cf6e3a5e
ALMAVictor <victormazoni0@gmail.com>
2026-02-11 12:40:01 -0300

    agora vai o whatsapp

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const WHATSAPP_NUMBER = '5528999491901';
-export const WHATSAPP_MESSAGE = "Olá! Vim pelo anúncio e gostaria de mais informações sobre energia solar";
+export const WHATSAPP_NUMBER = '554391483916';
+export const WHATSAPP_MESSAGE = "Olá! Vim pelo site da Strass Glass e gostaria de mais informações.";
